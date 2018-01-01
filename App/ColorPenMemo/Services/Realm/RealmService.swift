acdc0d3da877d51fb1c052c7ae215edb1e981335
tmokita <tmokita@gmail.com>
2018-01-02 02:12:02 +0900

    Realmの再セットアップ

---
+++ App/ColorPenMemo/Services/Realm/RealmService.swift
@@ -0,0 +1,19 @@
+//
+//  RealmService.swift
+//  ColorPenMemo
+//
+//  Created by TOMOHIKO OKITA on 2018/01/02.
+//  Copyright © 2018年 TOMOHIKO OKITA. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class RealmService {
+    
+    static func initialize() {
+        
+        
+    }
+    
+}
